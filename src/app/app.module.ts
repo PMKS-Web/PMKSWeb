27348a7894e87655cca9d3709a6dd369dd580b1a
AlexG1031 <adgalvan@wpi.edu>
2022-03-07 09:23:52 -0500

    Creation of Grid canvas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { GridComponent } from './grid/grid.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GridComponent
   ],
   imports: [
     BrowserModule
