c42ebc20f2a8c3fd9a1df06d673851af87ccfec3
AlexG1031 <adgalvan@wpi.edu>
2022-03-16 12:56:14 -0400

    Force is created on a link

--- src/app/model/force.ts
+++ src/app/model/force.ts
@@ -1,17 +1,37 @@
 import {Link} from "./link";
 import {Coord} from "../component/grid/coord/coord";
+import {AppConstants} from "../component/grid/app-constants/app-constants";
 
 export class Force {
   private _id: string
   private _link: Link;
   private _startCoord: Coord;
   private _endCoord: Coord;
+  private _forceLine: string;
+  private _forceArrow: string;
 
   constructor(id: string, link: Link, startCoord: Coord, endCoord: Coord) {
     this._id = id;
     this._link = link;
     this._startCoord = startCoord;
     this._endCoord = endCoord;
+    this._forceLine = 'M ' + startCoord.x.toString() + ' ' + startCoord.y.toString() + ' L '
+      + endCoord.x.toString() + ' ' + endCoord.y.toString() + ' Z';
+
+    // triangle (pointer) of the arrow
+    const angle = Math.atan2(endCoord.y - startCoord.y, endCoord.x - startCoord.x);
+    const a1 = angle - Math.PI / 6;
+    const a2 = angle + Math.PI / 6;
+    const triLen = 12 * AppConstants.scaleFactor;
+    const dx1 = Math.cos(a1) * triLen;
+    const dy1 = Math.sin(a1) * triLen;
+    const dx2 = Math.cos(a2) * triLen;
+    const dy2 = Math.sin(a2) * triLen;
+
+    // const triString = `M ${endX} ${endY} L ${endX - dx1} ${endY - dy1} L ${endX - dx2} ${endY - dy2} Z`;
+    this._forceArrow = 'M ' + endCoord.x.toString() + ' ' + endCoord.y.toString() +
+      ' L ' + (endCoord.x - dx1).toString() + ' ' + (endCoord.y - dy1).toString() +
+      ' L ' + (endCoord.x - dx2).toString() + ' ' + (endCoord.y - dy2).toString() + ' Z';
   }
 
   get id(): string {
@@ -44,5 +64,21 @@ export class Force {
 
   set endCoord(value: Coord) {
     this._endCoord = value;
+  }
+
+  get forceLine(): string {
+    return this._forceLine;
+  }
+
+  set forceLine(value: string) {
+    this._forceLine = value;
+  }
+
+  get forceArrow(): string {
+    return this._forceArrow;
+  }
+
+  set forceArrow(value: string) {
+    this._forceArrow = value;
   }
 }
