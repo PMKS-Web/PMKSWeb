8b7be8eeb40c9dbab7314bf0b6253a3f5df4f83d
AlexG1031 <adgalvan@wpi.edu>
2022-03-25 18:09:39 -0400

    fixed the highlighting within linkage table when clicking tabs

--- src/app/component/linkage-table/linkage-table.component.ts
+++ src/app/component/linkage-table/linkage-table.component.ts
@@ -13,15 +13,50 @@ export class LinkageTableComponent implements OnInit {
   @Input() links: Link[] = [];
   @Input() forces: Force[] = [];
 
+  private static jointButton: SVGElement;
+  private static linkButton: SVGElement;
+  private static forceButton: SVGElement;
+
   selectedTab: number = 0;
   // private static selectedTab: number = 0;
   constructor() { }
 
   ngOnInit(): void {}
 
+  ngAfterViewInit() {
+    LinkageTableComponent.jointButton = document.getElementById('jointButton') as unknown as SVGElement;
+    LinkageTableComponent.linkButton = document.getElementById('linkButton') as unknown as SVGElement;
+    LinkageTableComponent.forceButton = document.getElementById('forceButton') as unknown as SVGElement;
+  }
+
   setTab(tabNum: number) {
     this.selectedTab = tabNum;
-    const hello = ':)';
+    switch (tabNum) {
+      case 0:
+        LinkageTableComponent.jointButton.setAttribute('style',
+          'color: black; background-color: gray');
+        LinkageTableComponent.linkButton.setAttribute('style',
+          'color: gray; background-color: white');
+        LinkageTableComponent.forceButton.children[0].setAttribute('style',
+          'color: gray; background-color: white');
+        break;
+      case 1:
+        LinkageTableComponent.jointButton.setAttribute('style',
+          'color: gray; background-color: white');
+        LinkageTableComponent.linkButton.setAttribute('style',
+          'color: black; background-color: gray');
+        LinkageTableComponent.forceButton.setAttribute('style',
+          'color: gray; background-color: white');
+        break;
+      case 2:
+        LinkageTableComponent.jointButton.setAttribute('style',
+          'color: gray; background-color: white');
+        LinkageTableComponent.linkButton.setAttribute('style',
+          'color: gray; background-color: white');
+        LinkageTableComponent.forceButton.setAttribute('style',
+          'color: black; background-color: gray');
+        break;
+    }
     // LinkageTableComponent.selectedTab = $event.index;
   }
 
