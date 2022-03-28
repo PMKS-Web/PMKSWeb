d9df8a719d690dd789ad92418e9a551fb98d1f41
AlexG1031 <adgalvan@wpi.edu>
2022-03-28 15:06:14 -0400

    added simple TODO statement

--- src/app/component/grid/grid.component.ts
+++ src/app/component/grid/grid.component.ts
@@ -349,6 +349,7 @@ export class GridComponent implements OnInit, AfterViewInit {
                   const endCoordRaw = GridComponent.getMousePosition($event);
                   if (endCoordRaw === undefined) { return }
                   const endCoord = GridComponent.screenToGrid(endCoordRaw.x, endCoordRaw.y * -1);
+                  // TODO: Be sure the force added is at correct position for binary link
                   const force = new Force('F' + '1', GridComponent.selectedLink, startCoord, endCoord);
                   this.forces.push(force);
                   GridComponent.selectedLink.forces.push(force)
