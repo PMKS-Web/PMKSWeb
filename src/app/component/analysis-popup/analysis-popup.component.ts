d89c80428ba3dd39ff41d144858197702c6e3a1d
AlexG1031 <adgalvan@wpi.edu>
2022-04-08 12:46:27 -0400

    Buttons now showing up for Exporting, plotting, and showcasing equations

--- src/app/component/analysis-popup/analysis-popup.component.ts
+++ src/app/component/analysis-popup/analysis-popup.component.ts
@@ -7,6 +7,10 @@ import {AfterViewInit, Component, OnInit} from '@angular/core';
 })
 export class AnalysisPopupComponent implements OnInit, AfterViewInit {
   private static popUpWindow: SVGElement;
+  private static exportButton: SVGElement;
+  private static showPlotsButton: SVGElement;
+  private static showEqsButton: SVGElement;
+  selectedTab: number = 0;
 
   constructor() { }
 
@@ -15,6 +19,9 @@ export class AnalysisPopupComponent implements OnInit, AfterViewInit {
 
   ngAfterViewInit(): void {
     AnalysisPopupComponent.popUpWindow = document.getElementById('analysisPopup') as unknown as SVGElement;
+    AnalysisPopupComponent.exportButton = document.getElementById('exportButton') as unknown as SVGElement;
+    AnalysisPopupComponent.showPlotsButton = document.getElementById('showPlotsButton') as unknown as SVGElement;
+    AnalysisPopupComponent.showEqsButton = document.getElementById('showEqsButton') as unknown as SVGElement;
   }
 
   showAnalysis() {
@@ -23,5 +30,45 @@ export class AnalysisPopupComponent implements OnInit, AfterViewInit {
 
   closeAnalysis() {
     AnalysisPopupComponent.popUpWindow.style.display='none';
+  }
+
+
+  setTab(tabNum: number) {
+    this.selectedTab = tabNum;
+    // TODO: If possible, put this as hover within css
+    switch (tabNum) {
+      case 0:
+        AnalysisPopupComponent.exportButton.setAttribute('style',
+          'color: black; background-color: gray');
+        AnalysisPopupComponent.showPlotsButton.setAttribute('style',
+          'color: gray; background-color: white');
+        AnalysisPopupComponent.showEqsButton.setAttribute('style',
+          'color: gray; background-color: white');
+        break;
+      case 1:
+        AnalysisPopupComponent.exportButton.setAttribute('style',
+          'color: gray; background-color: white');
+        AnalysisPopupComponent.showPlotsButton.setAttribute('style',
+          'color: black; background-color: gray');
+        AnalysisPopupComponent.showEqsButton.setAttribute('style',
+          'color: gray; background-color: white');
+        break;
+      case 2:
+        AnalysisPopupComponent.exportButton.setAttribute('style',
+          'color: gray; background-color: white');
+        AnalysisPopupComponent.showPlotsButton.setAttribute('style',
+          'color: gray; background-color: white');
+        AnalysisPopupComponent.showEqsButton.setAttribute('style',
+          'color: black; background-color: gray');
+        break;
+    }
+  }
+
+  mouseOver(number: number) {
+
+  }
+
+  mouseOut(number: number) {
+
   }
 }
