94fc65fff91e2b9ac6692d43caeee0528e0d9d89
AlexG1031 <adgalvan@wpi.edu>
2022-04-08 14:57:58 -0400

    Included menu created when choosing which analysis to export

--- src/app/component/analysis-popup/analysis-popup.component.ts
+++ src/app/component/analysis-popup/analysis-popup.component.ts
@@ -13,6 +13,65 @@ export class AnalysisPopupComponent implements OnInit, AfterViewInit {
   selectedTab: number = 0;
   selectedAnalysis: string = '';
 
+  // utilizedLoops: string;
+  staticForcesCheck: boolean = false;
+  staticTorqueCheck: boolean = false;
+  dynamicForcesCheck: boolean = false;
+  dynamicTorqueCheck: boolean = false;
+
+  linKinJointCheck: boolean = false;
+  linKinJointPos: boolean = false;
+  linKinJointVel: boolean = false;
+  linKinJointAcc: boolean = false;
+  linKinLinkCheck: boolean = false;
+  dynamicAngLinkCheck: boolean = false;
+
+  linKinLinkPos: boolean = false;
+  linKinLinkVel: boolean = false;
+  linKinLinkAcc: boolean = false;
+  angKinLinkPos: boolean = false;
+  angKinLinkVel: boolean = false;
+  angKinLinkAcc: boolean = false;
+
+  icPositionsCheck: boolean = false;
+  linKinJointICCheck: boolean = false;
+  linKinJointICPos: boolean = false;
+  linKinJointICVel: boolean = false;
+  linKinLinkICPos: boolean = false;
+  linKinLinkICVel: boolean = false;
+  angKinLinkICCheck: boolean = false;
+  angKinLinkCheck: boolean = false;
+  angKinLinkICPos: boolean = false;
+  angKinLinkICVel: boolean = false;
+
+  staticJointPositionsCheck: boolean = false;
+  staticForcePositionsCheck: boolean = false;
+  dynamicForcePositionsCheck: boolean = false;
+  dynamicJointKinematicsCheck: boolean = false;
+  dynamicLinkKinematicsCheck: boolean = false;
+
+  // dynamicJointPositionsCheck: boolean;
+  // dynamicLinkPositionsCheck: boolean;
+  // dynamicJointVelocityCheck: boolean;
+  // dynamicLinkVelocityCheck: boolean;
+  // dynamicJointAccelerationCheck: boolean;
+  // dynamicLinkAccelerationCheck: boolean;
+
+  dynamicLinKinJointPos: boolean = false;
+  dynamicLinKinJointVel: boolean = false;
+  dynamicLinKinJointAcc: boolean = false;
+
+  dynamicLinKinLinkPos: boolean = false;
+  dynamicLinKinLinkVel: boolean = false;
+  dynamicLinKinLinkAcc: boolean = false;
+
+  dynamicAngKinLinkPos: boolean = false;
+  dynamicAngKinLinkVel: boolean = false;
+  dynamicAngKinLinkAcc: boolean = false;
+
+  allLoopCheck: boolean = false;
+  requiredLoopCheck: boolean = false;
+
   constructor() { }
 
   ngOnInit(): void {
@@ -72,5 +131,146 @@ export class AnalysisPopupComponent implements OnInit, AfterViewInit {
 
   mouseOut(number: number) {
 
+  }
+
+  setOption(e: any) {
+    // TODO: Figure out what is e's type?
+    switch (e.target.value) {
+      // kinematics section
+      // kin for joints
+      case 'linKinJointCheck':
+        this.linKinJointCheck = !this.linKinJointCheck;
+        break;
+      case 'linKinJointPos':
+        this.linKinJointPos = !this.linKinJointPos;
+        break;
+      case 'linKinJointVel':
+        this.linKinJointVel = !this.linKinJointVel;
+        break;
+      case 'linKinJointAcc':
+        this.linKinJointAcc = !this.linKinJointAcc;
+        break;
+      // kin for linear links
+      case 'linKinLinkCheck':
+        this.linKinLinkCheck = !this.linKinLinkCheck;
+        break;
+      case 'linKinLinkPos':
+        this.linKinLinkPos = !this.linKinLinkPos;
+        break;
+      case 'linKinLinkVel':
+        this.linKinLinkVel = !this.linKinLinkVel;
+        break;
+      case 'linKinLinkAcc':
+        this.linKinLinkAcc = !this.linKinLinkAcc;
+        break;
+      // kin for angular links
+      case 'angKinLinkCheck':
+        this.angKinLinkCheck = !this.angKinLinkCheck;
+        break;
+      case 'angKinLinkPos':
+        this.angKinLinkPos = !this.angKinLinkPos;
+        break;
+      case 'angKinLinkVel':
+        this.angKinLinkVel = !this.angKinLinkVel;
+        break;
+      case 'angKinLinkAcc':
+        this.angKinLinkAcc = !this.angKinLinkAcc;
+        break;
+      ///////////////////////////
+      // check for statics
+      case 'staticForcesCheck':
+        this.staticForcesCheck = !this.staticForcesCheck;
+        break;
+      case 'staticTorqueCheck':
+        this.staticTorqueCheck = !this.staticTorqueCheck;
+        break;
+      case 'staticForcePositionsCheck':
+        this.staticForcePositionsCheck = !this.staticForcePositionsCheck;
+        break;
+      case 'staticJointPositionsCheck':
+        this.staticJointPositionsCheck = !this.staticJointPositionsCheck;
+        break;
+      ///////////////
+      // check for dynamics
+      case 'dynamicForcesCheck':
+        this.dynamicForcesCheck = !this.dynamicForcesCheck;
+        break;
+      case 'dynamicTorqueCheck':
+        this.dynamicTorqueCheck = !this.dynamicTorqueCheck;
+        break;
+      case 'dynamicForcePositionsCheck':
+        this.dynamicForcePositionsCheck = !this.dynamicForcePositionsCheck;
+        break;
+      case 'dynamicJointKinematicsCheck':
+        this.dynamicJointKinematicsCheck = !this.dynamicJointKinematicsCheck;
+        break;
+      case 'dynamicLinKinJointPos':
+        this.dynamicLinKinJointPos = !this.dynamicLinKinJointPos;
+        break;
+      case 'dynamicLinKinJointVel':
+        this.dynamicLinKinJointVel = !this.dynamicLinKinJointVel;
+        break;
+      case 'dynamicLinKinJointAcc':
+        this.dynamicLinKinJointAcc = !this.dynamicLinKinJointAcc;
+        break;
+
+      case 'dynamicLinkKinematicsCheck':
+        this.dynamicLinkKinematicsCheck = !this.dynamicLinkKinematicsCheck;
+        break;
+      case 'dynamicLinKinLinkPos':
+        this.dynamicLinKinLinkPos = !this.dynamicLinKinLinkPos;
+        break;
+      case 'dynamicLinKinLinkVel':
+        this.dynamicLinKinLinkVel = !this.dynamicLinKinLinkVel;
+        break;
+      case 'dynamicLinKinLinkAcc':
+        this.dynamicLinKinLinkAcc = !this.dynamicLinKinLinkAcc;
+        break;
+
+      case 'dynamicAngLinkCheck':
+        this.dynamicAngLinkCheck = !this.dynamicAngLinkCheck;
+        break;
+      case 'dynamicAngKinLinkPos':
+        this.dynamicAngKinLinkPos = !this.dynamicAngKinLinkPos;
+        break;
+      case 'dynamicAngKinLinkVel':
+        this.dynamicAngKinLinkVel = !this.dynamicAngKinLinkVel;
+        break;
+      case 'dynamicAngKinLinkAcc':
+        this.dynamicAngKinLinkAcc = !this.dynamicAngKinLinkAcc;
+        break;
+      /////////
+      // check for IC
+      case 'icPositionsCheck':
+        this.icPositionsCheck = !this.icPositionsCheck;
+        break;
+      case 'linKinJointICCheck':
+        this.linKinJointICCheck = !this.linKinJointICCheck;
+        break;
+      case 'linKinJointICPos':
+        this.linKinJointICPos = !this.linKinJointICPos;
+        break;
+      case 'linKinJointICVel':
+        this.linKinJointICVel = !this.linKinJointICVel;
+        break;
+
+      case 'angKinLinkICCheck':
+        this.angKinLinkICCheck = !this.angKinLinkICCheck;
+        break;
+      case 'angKinLinkICPos':
+        this.angKinLinkICPos = !this.angKinLinkICPos;
+        break;
+      case 'angKinLinkICVel':
+        this.angKinLinkICVel = !this.angKinLinkICVel;
+        break;
+      ////////////
+      // check for loops
+      case 'allLoopCheck':
+        this.allLoopCheck = !this.allLoopCheck;
+        break;
+      case 'requiredLoopCheck':
+        this.requiredLoopCheck = !this.requiredLoopCheck;
+        break;
+    }
   }
 }
