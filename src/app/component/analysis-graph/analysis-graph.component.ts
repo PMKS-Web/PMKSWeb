7d0bd05b10158f8f573e946a42d77caace151ab5
Kohmei Kadoya <ajis.kkadoya@gmail.com>
2023-02-11 21:17:06 -0500

    Uppercase grid and time bugfix

--- src/app/component/analysis-graph/analysis-graph.component.ts
+++ src/app/component/analysis-graph/analysis-graph.component.ts
@@ -104,7 +104,7 @@ export class AnalysisGraphComponent implements OnInit, AfterViewInit, OnDestroy,
       x: {
         // show: false,
         formatter: function (val) {
-          return 'T = ' + (val - 1);
+          return 'T = ' + ((val - 1) / 125).toFixed(2) + 's';
         },
       },
       marker: {
