d8f11d4977514e889e053e799ef41e0f7d3ae97e
Kohmei Kadoya <ajis.kkadoya@gmail.com>
2023-03-20 16:40:20 -0400

    left tab open and close icons

--- src/app/component/tab/tab.component.ts
+++ src/app/component/tab/tab.component.ts
@@ -1,30 +1,9 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
-import { animate, state, style, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'app-tab',
   templateUrl: './tab.component.html',
   styleUrls: ['./tab.component.scss'],
-  animations: [
-    trigger('hover', [
-      // ...
-      state(
-        'default',
-        style({
-          opacity: 0.5,
-        })
-      ),
-      state(
-        'hover',
-        style({
-          opacity: 1,
-        })
-      ),
-
-      transition('default => hover', [animate('0.3s ease-in-out')]),
-      transition('hover => default', [animate('0.3s ease-in-out')]),
-    ]),
-  ],
 })
 export class TabComponent implements OnInit {
   @Input() tabLabel: string = 'NoName';
@@ -33,5 +12,6 @@ export class TabComponent implements OnInit {
   @Output() pressedEvent = new EventEmitter<number>();
 
   constructor() {}
+
   ngOnInit(): void {}
 }
