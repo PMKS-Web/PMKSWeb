e214d04021ab5a444701475ca3a02d33018a1429
Ansel Chang <ansel.chang@yahoo.com>
2023-06-02 10:39:32 +0900

    boilerplate for id validation

--- src/app/component/BLOCKS/editable-title/editable-title.component.ts
+++ src/app/component/BLOCKS/editable-title/editable-title.component.ts
@@ -24,13 +24,35 @@ export class EditableTitleComponent {
     this.editMode = true;
   }
 
+  // Check whether new id name is valid
+  // Return empty string if valid, or error message if not
+  validateNewID(newID: string): string {
+
+    // If the new ID only contains spaces, don't save it
+    if (newID === '') {
+      return 'The new ID cannot be empty.';
+    }
+
+    // If new ID is not purely alphanumeric, don't save it
+    if (false/*TODO*/) {
+      return "The new ID must be alphanumeric."
+    }
+
+    return "";
+  }
+
   saveNewID() {
-    //If the new ID only contains spaces, don't save it
-    if (this.newIDForm.value.newID!.trim() === '') {
-      NewGridComponent.sendNotification('The new ID cannot be empty.');
+
+    let newID = this.newIDForm.value.newID!.trim();
+
+    // If the new ID is not valid, send error notif and do not update to new id
+    let error = this.validateNewID(newID);
+    if (error !== '') {
+      NewGridComponent.sendNotification(error);
       this.editMode = false;
       return;
     }
+
     this.customIDService.addVisualID(this.originalID, this.newIDForm.value.newID!);
     console.log(this.customIDService.getVisualID(this.originalID));
     this.editMode = false;
