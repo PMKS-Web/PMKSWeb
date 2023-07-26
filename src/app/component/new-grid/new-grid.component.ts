e25631538e1b17815aca532dec714b6f4669565f
AlexG1031 <adgalvan@wpi.edu>
2023-07-26 12:20:56 -0400

    Context menu now only shows option to just unweld if a joint is welded. Believe we will need to still check the case of deleting joints and make sure this works properly...

--- src/app/component/new-grid/new-grid.component.ts
+++ src/app/component/new-grid/new-grid.component.ts
@@ -188,6 +188,16 @@ export class NewGridComponent {
         );
         break;
       case 'RevJoint':
+        if ((this.lastRightClick as RealJoint).isWelded) {
+          this.cMenuItems.push(
+              new cMenuItem(
+                  (this.lastRightClick as RealJoint).isWelded ? 'Unweld Location' : 'Weld Location',
+                  this.mechanismSrv.toggleSelectedWeldedJoint.bind(this.mechanismSrv),
+                  (this.lastRightClick as RealJoint).isWelded ? 'unweld_joint' : 'weld_joint'
+              )
+          );
+          break;
+        }
         this.cMenuItems.push(
           new cMenuItem(
             'Delete Joint',
