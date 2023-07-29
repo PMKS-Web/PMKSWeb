64fd5f8c2c7ae2c4494a6e1e36fb301c507540fe
Ansel Chang <ansel.chang@yahoo.com>
2023-07-28 21:24:15 -0400

    hover darken

--- src/app/services/synthesis/synthesis-constants.ts
+++ src/app/services/synthesis/synthesis-constants.ts
@@ -3,7 +3,9 @@ export class SynthesisConstants {
     public CIRCLE_RADIUS: number = 0.25;
     public LINK_CIRCLE_RADIUS: number = 0.4;
     public CIRCLE_COLOR: string = "rgb(255,255,255)";
+    public CIRCLE_COLOR_H: string = "rgb(245,245,245)";
     public LINK_COLOR: string = "rgb(219,183,108)";
+    public LINK_COLOR_H: string = "rgb(209, 173, 98)";
 
     constructor() {
 
