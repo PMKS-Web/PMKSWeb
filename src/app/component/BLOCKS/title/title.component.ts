785ff7168a99120188be0ae391ddad23c87598b8
Kohmei Kadoya <ajis.kkadoya@gmail.com>
2023-08-02 21:24:26 -0400

    Right panels updated with new style

--- src/app/component/BLOCKS/title/title.component.ts
+++ src/app/component/BLOCKS/title/title.component.ts
@@ -6,10 +6,36 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
   styleUrls: ['./title.component.scss'],
 })
 export class TitleBlock implements OnInit {
+  @Input() icon: string | undefined;
+  @Input() buttonLabel: string | undefined;
   @Input() description: string | undefined;
+
   @Output() nestedComponentChange: EventEmitter<number> = new EventEmitter<number>();
+
+  defaultIcon: string | undefined;
+  altIcon: string | undefined;
+  shownIcon: string | undefined;
 
   constructor() {}
 
-  ngOnInit(): void {}
+  toggleExpand() {
+    this.nestedComponentChange.emit(1);
+    if (this.shownIcon == this.defaultIcon) {
+      this.shownIcon = this.altIcon;
+    } else {
+      this.shownIcon = this.defaultIcon;
+    }
+  }
+
+  ngOnInit(): void {
+    this.defaultIcon = this.icon;
+    this.shownIcon = this.defaultIcon;
+    if (this.defaultIcon == 'expand_less') {
+      this.altIcon = 'expand_more';
+    } else if (this.defaultIcon == 'expand_more') {
+      this.altIcon = 'expand_less';
+    } else {
+      this.altIcon = this.defaultIcon;
+    }
+  }
 }
