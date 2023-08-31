d7be2f815bd8369b9076938f6554c9ded93576a2
Kohmei Kadoya <ajis.kkadoya@gmail.com>
2023-08-30 20:57:15 -0400

    disable button while talking to email server

--- src/app/component/right-panel/right-panel.component.ts
+++ src/app/component/right-panel/right-panel.component.ts
@@ -67,6 +67,8 @@ import { UrlGenerationService } from 'src/app/services/url-generation.service';
 export class RightPanelComponent {
   private analytics: Analytics = inject(Analytics);
 
+  public sendingEmail: boolean = false;
+
   commentForm = this.fb.group({
     comment: ['', Validators.required],
     email: ['', Validators.email],
@@ -323,8 +325,10 @@ export class RightPanelComponent {
   }
 
   async sendCommentEmail() {
+    this.sendingEmail = true;
     if (this.commentForm.invalid) {
       NewGridComponent.sendNotification('Please fill out the form correctly.');
+      this.sendingEmail = false;
       return;
     } else {
       let emailJSKey = '';
@@ -338,6 +342,7 @@ export class RightPanelComponent {
         NewGridComponent.sendNotification(
           'It looks like you are in a development environment. If this is not the case, please try again later or contact us directly at: gr-pmksplus@wpi.edu'
         );
+        this.sendingEmail = false;
         return;
       }
       emailjs.init(emailJSKey);
@@ -379,6 +384,7 @@ export class RightPanelComponent {
         .send('service_pg2k647', 'template_kfwdx5c', params)
         .then(() => {
           NewGridComponent.sendNotification('Message sent. Thank you for your feedback!');
+          this.sendingEmail = false;
           this.commentForm.reset();
         })
         .catch((error: any) => {
@@ -386,6 +392,7 @@ export class RightPanelComponent {
           NewGridComponent.sendNotification(
             'Message failed to send. Please try again later or contact us directly at: gr-pmksplus@wpi.edu'
           );
+          this.sendingEmail = false;
         });
     }
   }
